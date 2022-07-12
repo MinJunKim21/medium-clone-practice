e1f279b27170ba6b3062664f8b8b23da67409ac3
MinJunKim21 <jinju8012@gmail.com>
2022-07-12 19:16:21 +0900

    sanity에 comment 메뉴추가, Api token 설정, comment submit 하면 sanity로 데이터이동

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,12 +3,39 @@ import Header from '../../components/Header';
 import { sanityClient, urlFor } from '../../sanity';
 import { Post } from '../../typings';
 import PortableText from 'react-portable-text';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 interface Props {
   post: Post;
 }
 
 function Post({ post }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  // SubmitHandler라는 prop type을 사용하는거고, <IFormInput>을 전달하는거임
+
   return (
     <main>
       <Header />
@@ -65,14 +92,18 @@ function Post({ post }: Props) {
 
       <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-      <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col p-5 max-w-2xl mx-auto mb-10"
+      >
         <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
         <h4 className="text-3xl font-bold">Leave a comment below!</h4>
         <hr className="py-3 mt-2" />
-
+        <input {...register('_id')} type="hidden" name="_id" value={post._id} />
         <label className="block mb-5">
           <span className="text-gray-700">Name</span>
           <input
+            {...register('name', { required: true })}
             className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
             placeholder="Minjun Kim"
             type="text"
@@ -81,19 +112,40 @@ function Post({ post }: Props) {
         <label className="block mb-5">
           <span className="text-gray-700">Email</span>
           <input
+            {...register('email', { required: true })}
             className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="Minjun Kim"
-            type="text"
+            placeholder="email@email.com"
+            type="email"
           />
         </label>
         <label className="block mb-5">
           <span className="text-gray-700">Comment</span>
           <textarea
+            {...register('comment', { required: true })}
             className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring"
-            placeholder="Minjun Kim"
+            placeholder="Any comments"
             rows={8}
           />
         </label>
+
+        {/* validation fail 하면 errors가 리턴될거임 */}
+        <div className="flex flex-col p-5">
+          {errors.name && (
+            <span className="text-red-500 ">The Name Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500 ">The Email Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500 ">The Comment Field is required</span>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          value="Submit"
+          className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer "
+        />
       </form>
     </main>
   );
